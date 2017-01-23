517479313b1b1b1e3d1e5ac1a3ff55b9f323cd4f
CmdrUlle <cmdrulle@gmail.com>
2017-01-23 13:16:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: benny
- *
- * Created on 10. März 2016, 08:54
- */
-
 #include <cstdlib>
 #include <iostream>
 #include <stdio.h>
